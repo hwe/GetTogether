9c9effd75192f7d7e6589bebd368b72af24c0b2d
hwe <hannes@HW-NB.local>
2013-11-11 18:11:49 +0100

    Start Activity repariert

---
+++ GetTogether/src/de/tu_dresden/gettogether/view/MainActivity.java
@@ -0,0 +1,25 @@
+package de.tu_dresden.gettogether.view;
+
+import de.tu_dresden.gettogether.R;
+import de.tu_dresden.gettogether.R.layout;
+import de.tu_dresden.gettogether.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
