977c0697475cecb8a5f762b943b457bc74fd567e
hwe <hannes@wb0546.dip.tu-dresden.de>
2013-11-13 15:05:52 +0100

    Irgendwas

--- GetTogether/src/de/tu_dresden/gettogether/view/MainActivity.java
+++ GetTogether/src/de/tu_dresden/gettogether/view/MainActivity.java
@@ -1,11 +1,16 @@
 package de.tu_dresden.gettogether.view;
 
+import java.util.ArrayList;
+
 import de.tu_dresden.gettogether.R;
 import de.tu_dresden.gettogether.R.layout;
 import de.tu_dresden.gettogether.R.menu;
+import de.tu_dresden.gettogether.domain.Contact;
+import de.tu_dresden.gettogether.services.PhoneBookReader;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +18,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		ArrayList<Contact> contacts = PhoneBookReader.readAllContacts(getApplicationContext());
+		System.out.println(contacts.get(0).getFirstname());
 	}
 
 	@Override
